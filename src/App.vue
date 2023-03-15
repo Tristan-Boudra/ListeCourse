0b1af818eebddb5d16f9cd5eeb7f2832577ea361
Tristan-Boudra <boudratristan@gmail.com>
2023-03-15 10:31:13 +0100

    add, delete, update recipe

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,119 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <header>
+      <h1>Recipe Book</h1>
+    </header>
+    <form class="formAddRecipe" @submit.prevent="addRecipe">
+      <input type="text" v-model="newRecipe" placeholder="Enter a new recipe...">
+      <button class="btnBlue">Add</button>
+    </form>
+    <ul>
+      <li class="oneRecipe" v-for="(recipe, index) in recipe" :key="index">
+        <div v-if="editingRecipeIndex === index">
+          <input type="text" v-model="editingRecipeValue" @keydown.enter="updateRecipe(index)" @keydown.esc="cancelEditing">
+          <button class="btnWhite" @click="updateRecipe(index)">Save</button>
+          <button class="btnBlue" @click="cancelEditing">Cancel</button>
+        </div>
+        <div v-else>
+          {{ recipe }}
+          <button class="btnWhite" @click="editRecipe(index)">Edit</button>
+          <button class="btnBlue" @click="deleteRecipe(index)">Delete</button>
+        </div>
+      </li>
+    </ul>
   </main>
 </template>
 
+<script>
+  export default ({
+    name: 'AddRecipe',
+    data() {
+        return {
+          recipe: [],
+          newRecipe: '',
+          editingRecipeIndex: null,
+          editingRecipeValue: '',
+          recipeCount: 0
+        }
+    },
+    methods: {
+        addRecipe() {
+            if (this.newRecipe.trim() !== '') {
+            this.recipe.push(this.newRecipe.trim());
+            this.newRecipe = '';
+            this.updateRecipeCount();
+            }
+        },
+        deleteRecipe(index) {
+            this.recipe.splice(index, 1);
+            this.updateRecipeCount();
+        },
+        editRecipe(index) {
+            this.editingRecipeIndex = index;
+            this.editingRecipeValue = this.recipe[index];
+        },
+        updateRecipe(index) {
+            if (this.editingRecipeValue.trim() !== '') {
+            this.recipe[index] = this.editingRecipeValue.trim();
+            this.cancelEditing();
+            this.updateRecipeCount();
+            }
+        },
+        cancelEditing() {
+            this.editingRecipeIndex = null;
+            this.editingRecipeValue = '';
+        },
+        updateRecipeCount() {
+            this.recipeCount = this.recipe.length;
+        }
+    }
+  });
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.btnBlue {
+  background-color: #1E3041;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  border-radius: 5px;
+  cursor: pointer;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.btnWhite{
+  background-color: white;
+  color: #1E3041;
+  border: solid 1px #1E3041;
+  padding: 5px 10px;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-right: 10px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.oneRecipe{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 4px 0;
+  padding: 10px;
+  border: solid 1px #1E3041;
+  border-radius: 5px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+ul{
+  list-style: none;
+  margin-top: 30px;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.formAddRecipe{
+  margin-top: 30px;
+  display: flex;
+}
+
+.formAddRecipe input{
+  margin-right: 10px;
+  padding: 5px;
+  border: solid 1px #1E3041;
+  border-radius: 5px;
 }
 </style>
